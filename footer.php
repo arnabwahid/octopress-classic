11ad8cfb6a29e92182fa54d29ef4c27cc60b8318
Arnab Wahid <arnabwahid@gmail.com>
2018-10-08 09:15:14 +0530

    init
    footer link

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <footer role="contentinfo">
     <p>
         Copyright &copy; <?php echo date("Y"); ?> -
-        <span class="credit">Powered by <a href="https://github.com/arnabwahid/octopress">Octopress</a> theme
-				and <a href="https://wordpress.org/">WordPress</a></span>
+        <span class="credit">Powered by  <a href="https://wordpress.org/">WordPress</a> and <a
+                    href="https://github.com/arnabwahid/octopress">Octopress Theme</a></span>
     </p>
 </footer>
 <?php wp_footer(); ?>
