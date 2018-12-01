bb7e29efd16209fe5cdf82d8e68c266f45622e99
Arnab Wahid <arnabwahid@gmail.com>
2018-12-01 13:18:57 -0700

    Fix index.php

--- index.php
+++ index.php
@@ -1,35 +1,42 @@
+<?php
+/**
+ * Template: index.php
+ *
+ * @package    WordPress
+ * @subpackage wp-octopress-2
+ * @since      1.0
+ */
+
+?>
+
 <?php get_header(); ?>
 
 <div id="main">
-    <div id="content">
-        <div class="blog-index">
-			<?php if( have_posts() ): ?>
-				<?php while ( have_posts() ): the_post(); ?>
-					<?php get_template_part('content', get_post_format()); ?>
+	<div id="content">
+		<div class="blog-index">
+
+			<?php if ( have_posts() ) : ?>
+				<?php while ( have_posts() ) : ?>
+					<?php the_post(); ?>
+					<?php get_template_part( 'content', get_post_format() ); ?>
 				<?php endwhile; ?>
 			<?php endif; ?>
 
-
-
-
-			<?php if( !is_single() ): ?>
-                <div class="pagination">
-
-					<?php next_posts_link('← Older'); ?>
-
-                    <a href="/blog/archives">Blog Archives</a>
-
-					<?php previous_posts_link('Newer →'); ?>
-
-                </div>
+			<?php if ( ! is_single() ) : ?>
+				<div class="pagination">
+					<?php next_posts_link( '&larr; Older' ); ?>
+					<a href="/blog/archives"><?php esc_html_e( 'Blog Archives', 'wp-octopress' ); ?></a>
+					<?php previous_posts_link( 'Newer &rarr;' ); ?>
+				</div>
 			<?php endif; ?>
 
-        </div>
-        <aside class="sidebar thirds">
-			<?php dynamic_sidebar(); ?>
-        </aside>
-    </div>
-</div>
+		</div>
 
+		<aside class="sidebar thirds">
+			<?php dynamic_sidebar(); ?>
+		</aside>
+
+	</div>
+</div>
 
 <?php echo get_footer(); ?>
