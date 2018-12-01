1c8c85565601240c390c40f19cc87f4168457315
Arnab Wahid <arnabwahid@gmail.com>
2018-12-01 13:53:11 -0700

    Cleanup footer.php

--- footer.php
+++ footer.php
@@ -1,9 +1,18 @@
+<?php
+/**
+ * Part: footer.php
+ *
+ * @package    WordPress
+ * @subpackage wp-octopress
+ * @since      1.0
+ */
+
+?>
+
 <footer role="contentinfo">
-    <p>
-        Copyright &copy; <?php echo date("Y"); ?> -
-        <span class="credit">Powered by  <a href="https://wordpress.org/">WordPress</a> and <a
-                    href="https://github.com/arnabwahid/octopress">Octopress Theme</a></span>
-    </p>
+	<p>
+		<?php esc_html_e( 'Copyright &copy;', 'wp-octopress' ); ?> <?php echo wp_kses_post( date( 'Y' ) ); // @TODO: Convert to theme option. ?>
+	</p>
 </footer>
 <?php wp_footer(); ?>
 </body>
