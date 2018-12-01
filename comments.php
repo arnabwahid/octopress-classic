996f62426ca4b93b53534a38d81c6cedb30c82b3
Arnab Wahid <arnabwahid@gmail.com>
2018-12-01 14:07:35 -0700

    Fix comments.php

--- comments.php
+++ comments.php
@@ -1,45 +1,59 @@
-<?php if( post_password_required() ) {
-	echo '<p class="nocomments">This post is password protected. Enter the password to view comments.</p>';
-	return;
+<?php
+/**
+ * Part: comments.php
+ *
+ * @package    WordPress
+ * @subpackage wp-octopress
+ * @since      1.0
+ */
+
+?>
+
+<?php
+
+if ( post_password_required() ) {
+	echo wp_kses_post( '<p class="nocomments">' . __( 'This post is password protected. Enter the password to view comments.', 'wp-octopress' ) . '</p>' );
+		return;
 }
+
 ?>
 
 <div id="comments" class="comments-area">
+	<?php if ( have_comments() ) : ?>
 
-	<?php // You can start editing here -- including this comment! ?>
-
-	<?php if( have_comments() ): ?>
-        <h2 class="comments-title">
+		<h2 class="comments-title">
 			<?php
-				printf(
-					_n('One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'wp-octopress'),
-					number_format_i18n(get_comments_number()), '<span>' . get_the_title() . '</span>'
-				);
+
+			// translators.
+			echo wp_kses_post( sprintf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'wp-octopress' ), number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' ) );  // @codingStandardsIgnoreLine: @TODO: Fix _n here.
+
 			?>
-        </h2>
+		</h2>
 
-        <ul class="commentlist">
-			<?php wp_list_comments(array('callback' => 'octopress_comment', 'style' => 'ul')); ?>
-        </ul><!-- .commentlist -->
+		<ul class="commentlist">
+			<?php
 
-		<?php if( get_comment_pages_count() > 1 && get_option('page_comments') ): // are there comments to navigate through ?>
-            <nav id="comment-nav-below" class="navigation" role="navigation">
-                <h1 class="assistive-text section-heading"><?php _e('Comment navigation', 'wp-octopress'); ?></h1>
-                <div class="nav-previous"><?php previous_comments_link(__('&larr; Older Comments', 'wp-octopress')); ?></div>
-                <div class="nav-next"><?php next_comments_link(__('Newer Comments &rarr;', 'wp-octopress')); ?></div>
-            </nav>
-		<?php endif; // check for comment navigation ?>
+			wp_list_comments( array(
+				'callback' => 'octopress_comment',
+				'style'    => 'ul',
+			) );
 
-		<?php
-		/* If there are no comments and comments are closed, let's leave a note.
-		 * But we only want the note on posts and pages that had comments in the first place.
-		 */
-		if( !comments_open() && get_comments_number() ): ?>
-            <p class="nocomments"><?php _e('Comments are closed.', 'wp-octopress'); ?></p>
+			?>
+		</ul><!-- .commentlist -->
+
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+			<nav id="comment-nav-below" class="navigation" role="navigation">
+					<h1 class="assistive-text section-heading"><?php esc_html_e( 'Comment navigation', 'wp-octopress' ); ?></h1>
+					<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'wp-octopress' ) ); ?></div>
+					<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'wp-octopress' ) ); ?></div>
+			</nav>
 		<?php endif; ?>
 
-	<?php endif; // have_comments() ?>
+		<?php if ( ! comments_open() && get_comments_number() ) : ?>
+			<p class="nocomments"><?php esc_html_e( 'Comments are closed.', 'wp-octopress' ); ?></p>
+		<?php endif; ?>
+
+	<?php endif; // Has comments. ?>
 
 	<?php comment_form(); ?>
-
 </div>
