0582708209fe01684eb1bff083b38a6b7ce5ff7d
Arnab Wahid <arnabwahid@gmail.com>
2018-12-01 13:15:32 -0700

    Fix page.php

--- page.php
+++ page.php
@@ -1,38 +1,37 @@
 <?php
-	/**
-	 * The template for displaying all pages
-	 *
-	 * This is the template that displays all pages by default.
-	 * Please note that this is the WordPress construct of pages
-	 * and that other 'pages' on your WordPress site will use a
-	 * different template.
-	 *
-	 * @package    WordPress
-	 * @subpackage Octopress
-	 * @since      Octopress 1.0
-	 */
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package    WordPress
+ * @subpackage wp-octopress-2
+ * @since      1.0
+ */
 
-	get_header(); ?>
+?>
+
+<?php get_header(); ?>
 
 <div id="primary main" class="site-content">
-    <div id="content" role="main">
+	<div id="content" role="main">
 
-        <div>
-			<?php
-				while ( have_posts() ):
-					the_post();
-					?>
-					<?php get_template_part('content', 'page'); ?>
-					<?php comments_template('', true); ?>
-				<?php endwhile; // end of the loop. ?>
-        </div>
+		<div>
+			<?php while ( have_posts() ) : ?>
+				<?php the_post(); ?>
+				<?php get_template_part( 'content', 'page' ); ?>
+				<?php comments_template( '', true ); ?>
+			<?php endwhile; ?>
+		</div>
 
-
-        <aside class="sidebar thirds">
+		<aside class="sidebar thirds">
 			<?php dynamic_sidebar(); ?>
-        </aside>
-    </div><!-- #content -->
-</div><!-- #primary -->
+		</aside>
 
+	</div><!-- #content -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
