e387e68b220e1a5914d16dd78fedb17cd3ffdf81
Arnab Wahid <arnabwahid@gmail.com>
2018-12-02 01:04:45 -0700

    Easy custom 404

--- 404.php
+++ 404.php
@@ -14,15 +14,27 @@
 <div id="main">
 	<div id="content">
 		<div class="blog-index">
-			<article <?php post_class( 'hentry' ); ?> role="article">
-				<header>
-					<h1 class="entry-title"><?php esc_html_e( '404, Page Not Found', 'octopress-classic' ); ?></h1>
-				</header>
+			<?php if ( $page = get_page_by_title( '404' ) ) : // @codingStandardsIgnoreLine: I like it this way. ?>
+				<article <?php post_class( 'hentry' ); ?> role="article">
+					<header>
+						<h1 class="entry-title"><?php echo get_the_title( $page->ID ); ?></h1>
+					</header>
 
-				<div class="entry-content">
-					<?php get_search_form(); ?>
-				</div>
-			</article>
+					<div class="entry-content">
+						<?php echo wp_kses_post( apply_filters( 'the_content', $page->post_content ) ); ?>
+					</div>
+				</article>
+			<?php else : ?>
+				<article <?php post_class( 'hentry' ); ?> role="article">
+					<header>
+						<h1 class="entry-title"><?php esc_html_e( '404, Page Not Found', 'octopress-classic' ); ?></h1>
+					</header>
+
+					<div class="entry-content">
+						<?php get_search_form(); ?>
+					</div>
+				</article>
+			<?php endif; ?>
 		</div>
 
 		<aside class="sidebar thirds">
