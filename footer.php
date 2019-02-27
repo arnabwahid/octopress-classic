d78ef7ffcf9991b8110429de288bc64a7c6625f4
Arnab Wahid <arnabwahid@gmail.com>
2019-02-28 03:46:52 +0530

    footer links

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 
 <footer role="contentinfo">
 	<p>
-		<?php esc_html_e( 'Copyright &copy;', 'octopress-classic' ); ?> <?php echo wp_kses_post( date( 'Y' ) ); // @TODO: Convert to theme option. ?>
+		<?php esc_html_e( 'Copyright &copy;', 'octopress-classic' ); ?> <?php echo wp_kses_post( date( 'Y' ) ); // @TODO: Convert to theme option. ?> - <span class="credit">Powered by  <a href="https://wordpress.org/">WordPress</a> and <a href="https://github.com/arnabwahid/octopress">Octopress Theme</a></span>
 	</p>
 </footer>
 <?php wp_footer(); ?>
