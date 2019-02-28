aa858686206df15e327d819f69183e8c2f6e8312
Arnab Wahid <arnabwahid@gmail.com>
2019-03-01 00:25:31 +0530

    style fix minor

--- 404.php
+++ 404.php
@@ -15,16 +15,16 @@
 	<div id="content">
 		<div class="blog-index">
 			<?php if ($page = get_page_by_title('404')): // @codingStandardsIgnoreLine: I like it this way. ?>
-					<article <?php post_class('hentry');?> role="article">
-						<header>
-							<h1 class="entry-title"><?php echo get_the_title($page->ID); ?></h1>
-						</header>
+						<article <?php post_class('hentry');?> role="article">
+							<header>
+								<h1 class="entry-title"><?php echo get_the_title($page->ID); ?></h1>
+							</header>
 
-						<div class="entry-content">
-							<?php echo wp_kses_post(apply_filters('the_content', $page->post_content)); ?>
-						</div>
-					</article>
-				<?php else: ?>
+							<div class="entry-content">
+								<?php echo wp_kses_post(apply_filters('the_content', $page->post_content)); ?>
+							</div>
+						</article>
+					<?php else: ?>
 				<article <?php post_class('hentry');?> role="article">
 					<header>
 						<h1 class="entry-title"><?php esc_html_e('404', 'octopress-classic');?></h1>
